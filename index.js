c42fe943d2299c5d34baaf4ef7a869b9db771d7b
CaioAlmeida09 <Caio09092001@gmail.com>
2023-12-02 11:14:21 -0300

    alterando port

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ const port = process.env.PORT || 3333;
 app.get("/", (req, res) => {
   return res.json(products);
 });
-app.listen(3333, () => {
-  console.log("servidor rodando");
+app.listen(port, () => {
+  console.log(`servidor rodando na porta ${port}`);
 });
